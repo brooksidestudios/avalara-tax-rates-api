1173ae8cbfffb2c6739f8ed78745c86bb8623adf
Cornell Campbell <cornellsteven@gmail.com>
2016-05-24 11:14:53 -0500

    Convert uppercase booleans and nulls to lowercase to match PSR-2 standards

--- TaxRates.php
+++ TaxRates.php
@@ -6,22 +6,22 @@ namespace Brookside\TaxRates;
  *
  * @copyright Copyright (c) Brookside Studios
  * @link      https://github.com/brooksidestudios/avalara-tax-rates-api
- * @version   1.0.0
+ * @version   1.0.1
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 class TaxRates
 {
-    
+
     /**
      * The API base URL
      */
     const API_URL = 'https://taxrates.api.avalara.com:443/';
-    
+
     /**
      * Default country
      */
     const COUNTRY = 'USA';
-    
+
     /**
      * Error messages
      */
@@ -30,28 +30,28 @@ class TaxRates
     const ERROR_401     = 'Your authorization credentials were not provided or were invalid.';
     const ERROR_429     = 'Rate limiting has been exceeded. Try again later.';
     const ERROR_UNKNOWN = 'Unknown error with the tax rates api. Try again later.';
-    
+
     /**
      * The Avalara Tax Rates API key
      *
      * @var string
      */
     private $_apikey;
-    
+
     /**
      * Available actions
      *
      * @var string
      */
     private $_actions = array('postal', 'address');
-    
+
     /**
      * Available parameters
      *
      * @var string
      */
     private $_params = array('street', 'city', 'state', 'country', 'postal');
-    
+
     /**
      * Default constructor
      *
@@ -59,7 +59,7 @@ class TaxRates
      * @return void
      * @throws Exception
      */
-    public function __construct($apiKey = NULL)
+    public function __construct($apiKey = null)
     {
         if (isset($apiKey)) {
             $this->setApiKey($apiKey);
@@ -67,45 +67,45 @@ class TaxRates
             throw new \Exception('Missing API key');
         }
     }
-    
+
     /**
      * Retrieves a set of tax rates for a given address or postal code
      *
-     * @param array|string $params 
+     * @param array|string $params
      * @param string $action address|postal
      * @return array
      * @throws Exception
      */
-    public function getRates($params = array(), $action = NULL)
+    public function getRates($params = array(), $action = null)
     {
         if (is_string($params)) {
             $params = array('postal' => $params);
         }
-        
+
         if ( ! isset($params['country'])) {
             $params['country'] = self::COUNTRY;
         }
-        
+
         $action = (isset($action) && in_array($action, $this->getActions()) ? $action : $this->getActionFromParams($params));
-        
+
         if ( ! $this->hasRequiredParamsForAction($action, $params)) {
             throw new \Exception('You are missing one or more required parameters.');
         }
-        
+
         return $this->_makeCall($action, $params);
     }
-    
+
     /**
      * API key setter
      *
-     * @param string $apiKey 
+     * @param string $apiKey
      * @return void
      */
     private function setApiKey($apiKey)
     {
         $this->_apikey = $apiKey;
     }
-    
+
     /**
      * API key getter
      *
@@ -115,7 +115,7 @@ class TaxRates
     {
         return $this->_apikey;
     }
-    
+
     /**
      * Available actions getter
      *
@@ -125,11 +125,11 @@ class TaxRates
     {
         return $this->_actions;
     }
-    
+
     /**
      * Attempts to get the proper action from parameters passed
      *
-     * @param array $params 
+     * @param array $params
      * @return string
      */
     private function getActionFromParams($params = array())
@@ -139,15 +139,15 @@ class TaxRates
                 return 'postal';
             }
         }
-        
+
         return 'address';
     }
-    
+
     /**
      * Checks for required fields for a given action
      *
-     * @param string $action 
-     * @param array $params 
+     * @param string $action
+     * @param array $params
      * @return boolean
      */
     private function hasRequiredParamsForAction($action, $params)
@@ -155,23 +155,23 @@ class TaxRates
         if ($action == 'address') {
             foreach ($this->_params as $param) {
                 if ( ! isset($params[$param])) {
-                    return FALSE;
+                    return false;
                 }
             }
         } else {
             if ( ! isset($params['country'], $params['postal'])) {
-                return FALSE;
+                return false;
             }
         }
-        
-        return TRUE;
+
+        return true;
     }
-    
+
     /**
      * Method that makes the actual call to the API endpoint
      *
-     * @param string $action 
-     * @param array $params 
+     * @param string $action
+     * @param array $params
      * @return array
      * @throws Exception
      */
@@ -180,32 +180,32 @@ class TaxRates
         if ( ! isset($this->_apikey)) {
             throw new \Exception('Invalid api key');
         }
-        
+
         $authMethod = '?apikey=' . urlencode($this->getApiKey());
-        
-        $paramString = NULL;
-        
+
+        $paramString = null;
+
         if (isset($params) && is_array($params)) {
             $paramString = '&' . http_build_query($params);
         }
-        
+
         $apiCall = self::API_URL . $action . $authMethod . $paramString;
-        
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $apiCall);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
         curl_setopt($ch, CURLOPT_TIMEOUT, 90);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
         $jsonData = curl_exec($ch);
-        
+
         if (curl_errno($ch) || ! $jsonData) {
             throw new \Exception('Error: _makeCall() - cURL error: ' . curl_error($ch));
         }
-        
+
         $info = curl_getinfo($ch);
-        
+
         if (preg_match('/^4/', $info['http_code'])) {
             switch ($info['http_code']) {
                 case 400:
@@ -215,26 +215,26 @@ class TaxRates
                         $error = self::ERROR_400A;
                     }
                     break;
-                    
+
                 case 401:
                     $error = self::ERROR_401;
                     break;
-                    
+
                 case 429:
                     $error = self::ERROR_429;
                     break;
-                    
+
                 default:
                     $error = self::ERROR_UNKNOWN;
                     break;
             }
-            
+
             throw new \Exception($error);
         }
-        
+
         curl_close($ch);
-        
-        return json_decode($jsonData, TRUE);
+
+        return json_decode($jsonData, true);
     }
-    
+
 }
